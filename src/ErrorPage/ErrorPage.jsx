96207051301900a4ac9599492705d5f379c73189
biruni60 <aburaihanalbiruni007@gmail.com>
2024-02-20 03:52:45 +0600

    set up done and navbar added

---
+++ src/ErrorPage/ErrorPage.jsx
@@ -0,0 +1,11 @@
+
+
+const ErrorPage = () => {
+    return (
+        <div>
+            <h2>404 | PAGE NOT FOUND </h2>
+        </div>
+    );
+};
+
+export default ErrorPage;
---
+++ src/Navbar/Navbar.jsx
@@ -0,0 +1,42 @@
+import { NavLink } from "react-router-dom";
+
+
+const Navbar = () => {
+    const links= <>
+       <NavLink to="/"><li><a>HOME</a></li></NavLink>
+       <NavLink to="/"><li><a>ADD PRODUCT</a></li></NavLink>
+       <NavLink to="/"><li><a>MY CART</a></li></NavLink>
+    </>
+    return (
+        <div className="navbar bg-base-300 text-yellow-600">
+        <div className="navbar-start">
+          <div className="dropdown">
+            <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
+              <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
+            </div>
+            <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
+             {
+                links
+             }
+            </ul>
+          </div>
+          <div className="flex gap-2 justify-center items-center">
+            <img className="w-16 flex-1" src="https://i.ibb.co/FzBC5zj/Yellow-and-Black-Online-Shop-Business-Logo-removebg-preview.png" alt="" />
+            <p className="text-xl">ARAB CLOTHING</p>
+          </div>
+        </div>
+        <div className="navbar-center hidden lg:flex">
+          <ul className="menu menu-horizontal px-1">
+     {
+        links
+     }
+          </ul>
+        </div>
+        <div className="navbar-end">
+          <a className="btn bg-transparent text-yellow-600 text-lg">LOG IN</a>
+        </div>
+      </div>
+    );
+};
+
+export default Navbar;
